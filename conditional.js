558b45b50df4f344fdf4c06497674cdfe77c5c48
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-08-01 23:43:53 +0600

    if-else

--- conditional.js
+++ conditional.js
@@ -1,6 +1,17 @@
-var iphonePrice = 79500;
-var myBuget = 95000;
+// var iphonePrice = 79500;
+// var myBuget = 95000;
 
-if (iphonePrice < myBuget) {
-    console.log("iphone ami kinbo ")
-};
+// if (iphonePrice > myBuget) {
+//     console.log("iphone ami kinbo ");
+// }
+// else {
+//     console.log("ami duraiya bari asbo");
+// }
+var gariPrice = 5000;
+var buy = 4000;
+if (gariPrice < buy) {
+    console.log("ami kinbo");
+}
+else {
+    console.log("kilam kinto");
+}
