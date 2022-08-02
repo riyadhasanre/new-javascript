f6b7bdba7fe8e08c8fe4de1c8ef8b17ae826c88b
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-08-02 14:49:45 +0600

    arry

---
+++ multi.js
@@ -0,0 +1,15 @@
+var destination = ["sajek", "coxbazar", "sorargoan", "himchori"];
+// man ber korar system
+console.log(destination[2])
+// man set korarr system 
+destination[2] = "japan";
+console.log(destination);
+// man er position ber kora 
+var position = destination.indexOf("himchori");
+console.log(position);
+// man add kora 
+destination.push("bhutan","limasol");
+console.log(destination);
+// man biyog kora 
+destination.pop("limasol");
+console.log(destination);
